44c4a0b16d9d1ce58fed8a459089db8921bf5899
Benjamin Siev <benjamin.siev@wsu.edu>
2026-04-18 17:44:44 -0700

    Setup SFML and test file

---
+++ PA9/main.cpp
@@ -0,0 +1,21 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode({ 1280, 720 }), "SFML works!");
+    sf::RectangleShape shape(sf::Vector2f(200, 75));
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        while (const std::optional event = window.pollEvent())
+        {
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+}
